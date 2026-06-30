4ecf7953a97192a4db0bf88e0457ea7044c7fb62
kikisrbinoska <srbinoskakristina07@gmail.com>
2026-06-30 15:35:52 +0200

    Fixed env

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker compose -f "$ROOT_DIR/docker-compose.yml" --env-file "$ROOT_DIR/env" up --build "$@"
+docker compose -f "$ROOT_DIR/docker-compose.yml" --env-file "$ROOT_DIR/.env" up --build "$@"
