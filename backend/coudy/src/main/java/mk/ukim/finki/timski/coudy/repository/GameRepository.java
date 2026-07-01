f65858ec5d717e09d9b6eb2a4afc3acd8d288489
bozankailieva <bozankailieva1234@gmail.com>
2026-07-01 16:41:12 +0200

    GamesFixData

--- backend/coudy/src/main/java/mk/ukim/finki/timski/coudy/repository/GameRepository.java
+++ backend/coudy/src/main/java/mk/ukim/finki/timski/coudy/repository/GameRepository.java
@@ -4,6 +4,10 @@ import mk.ukim.finki.timski.coudy.model.domain.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
+
+    Optional<Game> findByName(String name);
 }
